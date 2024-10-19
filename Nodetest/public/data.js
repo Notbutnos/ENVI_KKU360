0bff1a2dd80126845f1b7e3c772468977e4418ab
Notbutnos <oomsinx6@gmail.com>
2024-10-19 23:36:17 +0700

    my commit

---
+++ Nodetest/public/data.js
@@ -0,0 +1,490 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "โถงหลัก",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.028576521000008626,
+        "pitch": 0,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.35791337962401926,
+          "pitch": -0.07671328149847412,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.4614610782543869,
+          "pitch": -0.03914090217985233,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.403377060729916,
+          "pitch": -0.037355846635701795,
+          "title": "ลานจอดรถ",
+          "text": ""
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "ด้านนอกอาคาร",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.82388717696389,
+        "pitch": 0.1427980770414088,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.022623333027819825,
+          "pitch": -0.06083714184384981,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "หน้าบันได",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8325737873482693,
+          "pitch": -0.10866731327359425,
+          "rotation": 0,
+          "target": "4-1-2"
+        },
+        {
+          "yaw": -0.40220472425291476,
+          "pitch": 0.03568136457761639,
+          "rotation": 6.283185307179586,
+          "target": "3--1"
+        },
+        {
+          "yaw": 2.8705984227587455,
+          "pitch": 0.0633109968312695,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--1",
+      "name": "ทางเดินชั้น 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.118086414392872,
+          "pitch": 0.005191557803874147,
+          "rotation": 6.283185307179586,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-1-2",
+      "name": "บันไดเชื่อมชั้น1-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.734645244693553,
+        "pitch": -0.15819323492153536,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.148007536958458,
+          "pitch": -0.45232556985235206,
+          "rotation": 6.283185307179586,
+          "target": "5--2"
+        },
+        {
+          "yaw": 0.11815569277838023,
+          "pitch": 0.07665498749612532,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--2",
+      "name": "หน้าบันไดชั้น 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -1.4286205501571612,
+        "pitch": 0.1292678569245762,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.983466362950411,
+          "pitch": 0.10440222936253818,
+          "rotation": 6.283185307179586,
+          "target": "6--2"
+        },
+        {
+          "yaw": -1.8431031044361212,
+          "pitch": 0.2638523118656657,
+          "rotation": 0,
+          "target": "4-1-2"
+        },
+        {
+          "yaw": -0.6057596800384459,
+          "pitch": 0.033608741529404185,
+          "rotation": 0,
+          "target": "8--2"
+        },
+        {
+          "yaw": -2.4772359639989894,
+          "pitch": 0.06854055695406203,
+          "rotation": 0,
+          "target": "10-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--2",
+      "name": "ทางเดินแยกซ้ายชั้น 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.04659840951945071,
+          "pitch": -0.06865508410213117,
+          "rotation": 0,
+          "target": "7--2"
+        },
+        {
+          "yaw": -3.0931854363934104,
+          "pitch": 0.01617556010897303,
+          "rotation": 0,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7--2",
+      "name": "สุดทางเดินแยกซ้ายชั้น 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0896691613567544,
+          "pitch": 0.021251192314666767,
+          "rotation": 0,
+          "target": "6--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8--2",
+      "name": "ทางเดินแยกขวาชั้น 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1500,
+      "initialViewParameters": {
+        "yaw": -2.9944493039449522,
+        "pitch": 0.2105508200582289,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.092375415796843,
+          "pitch": 0.07181525656653598,
+          "rotation": 0,
+          "target": "9--2"
+        },
+        {
+          "yaw": 0.017405055869220476,
+          "pitch": -0.0639132889284113,
+          "rotation": 0,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9--2",
+      "name": "สุดทางเดินแยกขวาชั้น 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 2.0000328799873426,
+        "pitch": -0.029051397064707274,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2520636157155511,
+          "pitch": -0.05805149720879399,
+          "rotation": 0,
+          "target": "8--2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-",
+      "name": "นอกอาคารด้านหลัง",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -3.1385872579433567,
+        "pitch": 0.14927044453738603,
+        "fov": 1.3365071038314758
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5823809875530284,
+          "pitch": -0.2238354599230341,
+          "rotation": 0,
+          "target": "5--2"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
